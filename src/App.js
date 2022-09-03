79dc77ec7b044f8c143f0f41ed4ffc2b53abddfa
Faraz Ahmed Abir <62275863+farazabir@users.noreply.github.com>
2022-09-03 23:41:54 +0600

    commit message

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import SpeedApp from './components/SpeedApp';
+
+function App() {
+ 
+
+  return (
+    <div className='' >
+      <Navbar/>
+      <SpeedApp/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { RiFacebookCircleLine } from "react-icons/ri";
+import { FaGithubSquare } from "react-icons/fa";
+const Footer = () => {
+  return (
+    <div>
+      <div className="d-flex justify-content-center mt-5 pt-5">
+        <h3 className="underlined">
+          Made By <span className="underlined">Faraz Ahmed Abir</span>
+        </h3>
+      </div>
+
+      <div className="mt-1 d-flex justify-content-center">
+        <a href="https://www.facebook.com/farazahamed.abir">
+          <RiFacebookCircleLine size={20} className="ml-4" />
+        </a>
+
+        <a href="https://github.com/farazabir">
+          <FaGithubSquare size={20} className="ms-4" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
